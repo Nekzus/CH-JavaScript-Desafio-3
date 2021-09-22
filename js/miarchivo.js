41b6634783a46af8417d340d47e7a7ce3fae7911
Nekzus <85507589+Nekzus@users.noreply.github.com>
2021-09-21 23:14:25 -0300

    Se aplica lowerCase a usuario ingresado

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -4,34 +4,34 @@ Una vez se comprueba la legitimidad del usuario, mediante un if y contadores, se
 
 
 /* El usuario ingresa sus datos de cuenta.
-Usuarios Registrados 'Mathias' y 'Natalia'. */
-let usuario = prompt("Ingresar Usuario - 'Mathias' o 'Natalia'.");
+Usuarios Registrados 'mathias' y 'natalia'. */
+let usuario = prompt("Ingresar Usuario - 'mathias' o 'natalia'.");
 
 // Contadores de sesiones iniciadas.
-let i = 0; // cuenta sesiones 'Mathias'.
-let j = 0; // cuenta sesiones 'Natalia'.
+let i = 0; // cuenta sesiones 'mathias'.
+let j = 0; // cuenta sesiones 'natalia'.
 
 // Se verifica que el campo de usuario no este vacio, en tal caso se vuelve a solicitar se ingrese usuario.
 while(usuario != null) {
-    switch(usuario) {
-        case "Mathias":  // si el usuario es Mathias ejecuta if, de lo contrario verifica proximo usuario.
+    switch(usuario.toLowerCase()) {
+        case "mathias":  // si el usuario es mathias ejecuta if, de lo contrario verifica proximo usuario.
             if(i < 2) { // Si la cantidad de sesiones no supera 2 continua con el conteo de sesiones, de lo contrario devuelve mensaje de número de sesiones superadas.
                 i++; // Se incrementa contador de sesiones.
                 alert(`Bienvenido Mathias Barbosa \nTipo de cuenta: Tutor \nSesiones: ${i} \nSesiones restantes: ${2 - i}`);
             }else{
-            alert(`${usuario} superaste el número de sesiones permitidas.`);
+            alert(`Mathias superaste el número de sesiones permitidas.`);
             }
             break;
-        case "Natalia":  // si el usuario es Natalia ejecuta if, de lo contrario devuelve mensaje de error 'usuario no existe'.
+        case "natalia":  // si el usuario es natalia ejecuta if, de lo contrario devuelve mensaje de error 'usuario no existe'.
             if(j < 2) {  // Si la cantidad de sesiones no supera 2 continua con el conteo de sesiones, de lo contrario devuelve mensaje de número de sesiones superadas.
                 j++; // Se incrementa contador de sesiones.
                 alert(`Bienvenida Natalia Machado \nTipo de cuenta: Profesor \nSesiones: ${j} \nSesiones restantes: ${2 - j}`);
             }else{
-            alert(`${usuario} superaste el número de sesiones permitidas.`);
+            alert(`Natalia superaste el número de sesiones permitidas.`);
             }
             break;
         default:
             alert(`Error: el usuario ingresado "${usuario}" no existe.`);
     }
-        usuario = prompt("Vuelve a ingresar Usuario - 'Mathias' o 'Natalia'."); // Si el usuario no existe, o esta vacio, vuelve a solicitar el ingreso.
+        usuario = prompt("Vuelve a ingresar Usuario - 'mathias' o 'natalia'."); // Si el usuario no existe, o esta vacio, vuelve a solicitar el ingreso.
 }
